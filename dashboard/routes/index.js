898ab07be4380dd6d4b3f38e0d46c382d3f9f04f
Solace <monkdev9@gmail.com>
2026-01-05 21:59:14 +0000

    Implement TOS and Privacy Policy routes
    Added routes for Terms of Service and Privacy Policy with rendering logic.

--- dashboard/routes/index.js
+++ dashboard/routes/index.js
@@ -2,14 +2,37 @@ const express = require("express"),
   CheckAuth = require("../auth/CheckAuth"),
   router = express.Router();
 
+// Home
 router.get("/", CheckAuth, async (req, res) => {
-  res.redirect("/selector");
+  res.render("home", {
+    user: req.userInfos,
+    currentURL: `${req.client.config.DASHBOARD.baseURL}${req.originalUrl}`,
+  });
 });
 
+// Selector
 router.get("/selector", CheckAuth, async (req, res) => {
   res.render("selector", {
     user: req.userInfos,
-    currentURL: `${req.client.config.DASHBOARD.baseURL}/${req.originalUrl}`,
+    currentURL: `${req.client.config.DASHBOARD.baseURL}${req.originalUrl}`,
+  });
+});
+
+// Terms of Service
+router.get("/tos", CheckAuth, async (req, res) => {
+  res.render("tos", {
+    user: req.userInfos,
+    currentURL: `${req.client.config.DASHBOARD.baseURL}${req.originalUrl}`,
+    tosUpdatedAt: "January 5, 2026",
+  });
+});
+
+// Privacy Policy
+router.get("/privacy", CheckAuth, async (req, res) => {
+  res.render("privacy", {
+    user: req.userInfos,
+    currentURL: `${req.client.config.DASHBOARD.baseURL}${req.originalUrl}`,
+    privacyUpdatedAt: "January 5, 2026",
   });
 });
 
